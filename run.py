d206625eb158c44df373b1b21b9a2242cf3b2dc7
etherealsheep <g.perrotta79@gmail.com>
2022-11-29 15:20:11 +0000

    Add scenario selection function

--- run.py
+++ run.py
@@ -4,6 +4,12 @@
 import random
 
 
+def determine_scenario():
+    scenarios = ["TAKE AND HOLD", "BATTLE FOR THE TOWER", "DEFENDING THE VILLAGE", "VAGON TRAIN"]
+    scenario_choice = random.choice(scenarios)
+    return scenario_choice
+
+
 def number_turns():
     nTurns = [6, 7, 8]
     choice = random.choice(nTurns)
@@ -16,9 +22,11 @@ def determine_attacker():
     return choice
 
 
+scenario = determine_scenario()
 number_of_turns = number_turns()
 player_role = determine_attacker()
 
 
-print(f"The battle will be {number_of_turns} long.")
-print(f"Player 1 is the {player_role} and Player 2 is the {player_role}.")
+print(f"The battle between will be: {scenario}. \n")
+print(f"The battle will end {number_of_turns}. \n")
+print(f"Player 1 is the {player_role} and Player 2 is the {player_role}. \n")
