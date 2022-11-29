624dbda1ba2acb4619cb5333af4a4c7f84efb2db
etherealsheep <g.perrotta79@gmail.com>
2022-11-29 15:02:42 +0000

    Add player role initial function

--- run.py
+++ run.py
@@ -10,7 +10,15 @@ def number_turns():
     return choice
 
 
+def determine_attacker():
+    player1 = ["Attacker", "Defender"]
+    choice = random.choice(player1)
+    return choice
+
+
 number_of_turns = number_turns()
+player_role = determine_attacker()
 
 
 print(f"The battle will be {number_of_turns} long.")
+print(f"Player 1 is the {player_role} and Player 2 is the {player_role}.")
