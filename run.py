5d81462ee1101ae52f35328ff8fd8a5ce80e2401
etherealsheep <g.perrotta79@gmail.com>
2022-11-29 15:49:23 +0000

    Add players input

--- run.py
+++ run.py
@@ -2,10 +2,22 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
+import time
+
+
+p1 = input("Please enter Player1 Army: \n").upper()
+p2 = input("Please enter Player1 Army: \n").upper()
+print(f"{p1} vs {p2}.")
+time.sleep(1)
 
 
 def determine_scenario():
-    scenarios = ["TAKE AND HOLD", "BATTLE FOR THE TOWER", "DEFENDING THE VILLAGE", "VAGON TRAIN"]
+    scenarios = [
+        "TAKE AND HOLD", 
+        "BATTLE FOR THE TOWER", 
+        "DEFENDING THE VILLAGE", 
+        "VAGON TRAIN"
+    ]
     scenario_choice = random.choice(scenarios)
     return scenario_choice
 
@@ -27,6 +39,8 @@ number_of_turns = number_turns()
 player_role = determine_attacker()
 
 
-print(f"The battle between will be: {scenario}. \n")
-print(f"The battle will end {number_of_turns}. \n")
+print(f"The battle between {p1} and {p2} will be: {scenario}. \n")
+time.sleep(1)
+print(f"The battle will end after {number_of_turns} turns. \n")
+time.sleep(1)
 print(f"Player 1 is the {player_role} and Player 2 is the {player_role}. \n")
