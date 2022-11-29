dc88db4e575a387fddbaed9b7b63f846fcec0df6
etherealsheep <g.perrotta79@gmail.com>
2022-11-29 15:50:19 +0000

    Fix timers

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ import time
 p1 = input("Please enter Player1 Army: \n").upper()
 p2 = input("Please enter Player1 Army: \n").upper()
 print(f"{p1} vs {p2}.")
-time.sleep(1)
+time.sleep(2)
 
 
 def determine_scenario():
@@ -40,7 +40,7 @@ player_role = determine_attacker()
 
 
 print(f"The battle between {p1} and {p2} will be: {scenario}. \n")
-time.sleep(1)
+time.sleep(2)
 print(f"The battle will end after {number_of_turns} turns. \n")
-time.sleep(1)
+time.sleep(2)
 print(f"Player 1 is the {player_role} and Player 2 is the {player_role}. \n")
