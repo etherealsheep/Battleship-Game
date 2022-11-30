abf4100286a3ddcbb44309ea0f7801b285f52863
etherealsheep <g.perrotta79@gmail.com>
2022-11-30 19:39:29 +0000

    Remove random import and functions

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import time
-import random
 
 objects = {
         'attacker': [
@@ -109,42 +108,3 @@ def banner():
     time.sleep(3)
 
 
-p1 = input("Please enter Player1 Army: \n").upper()
-p2 = input("Please enter Player2 Army: \n").upper()
-print(f"{p1} vs {p2}.")
-time.sleep(2)
-
-
-def determine_scenario():
-    scenarios = [
-        "TAKE AND HOLD",
-        "BATTLE FOR THE TOWER",
-        "DEFENDING THE VILLAGE",
-        "VAGON TRAIN"
-    ]
-    scenario_choice = random.choice(scenarios)
-    return scenario_choice
-
-
-def number_turns():
-    nTurns = [6, 7, 8]
-    choice = random.choice(nTurns)
-    return choice
-
-
-def determine_attacker():
-    player1 = ["Attacker", "Defender"]
-    choice = random.choice(player1)
-    return choice
-
-
-scenario = determine_scenario()
-number_of_turns = number_turns()
-player_role = determine_attacker()
-
-
-print(f"The battle between {p1} and {p2} will be: {scenario}. \n")
-time.sleep(2)
-print(f"The battle will end after {number_of_turns} turns. \n")
-time.sleep(2)
-print(f"{p1} Army is the {player_role}. {p2} Army is the {player_role}. \n")
